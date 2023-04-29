470cbdfba4c4559d71f8eb697751ec866e3d381a
Mennaamohamed25 <Mennaa.mohamedd25@gmail.com>
2023-04-29 14:18:43 +0300

    quoit app

---
+++ main.js
@@ -0,0 +1,33 @@
+
+
+var quotes = [
+    {'author': 'Jim Rohn', 
+     'quote': 'Beware of what you become in pursuit of what you want.'
+    },
+    {'author': 'Epictetus', 
+     'quote': 'Its not what happens to you, but how you react to it that matters.'
+    },
+    {'author': 'Frank Sinatra', 
+     'quote': 'The best revenge is massive success.'
+    },
+    {'author': 'Wayne Gretzy', 
+     'quote': 'You miss 100% of the shots you dont take.'
+    },
+    {'author': 'Nelson Mandela', 
+     'quote': 'Resentment is like drinking poison and waiting for your enemies to die.'
+    },
+    {'author': 'Elbert Hubbard', 
+     'quote': 'Do not take life too seriously. You will not get out alive.'
+    },
+];
+
+
+ 
+function changeQuote(){
+
+
+    var random = Number.parseInt(Math.random()*quotes.length);
+    document.getElementById('quote').innerHTML = quotes[random].quote;
+    document.getElementById('author').innerHTML = quotes[random].author;
+
+}
